e9ec6aa9ccc30be3ee4bb54d57f260d5c73abfed
Abhi Yerra <abhi@berkeley.edu>
2020-08-09 19:35:33 -0700

    Added Rust Code

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use structopt::StructOpt;
+use airtable_api::Airtable;
+
+#[derive(StructOpt)]
+struct Cli {
+    api_key: String,
+    base_key: String,
+    table: String,
+    view: String,
+    key_field: String,
+    value_field: String,
+}
+
+#[tokio::main]
+pub async fn main() {
+    let args = Cli::from_args();
+    let airtable = Airtable::new(&args.api_key, &args.base_key);
+    let records = airtable
+        .list_records(&args.table, &args.view)
+        .await
+        .unwrap();
+
+    records.iter().enumerate().for_each(|(_, record)| {
+        println!("{}={}",
+            record.fields[&args.key_field].as_str().unwrap(),
+            record.fields[&args.value_field].as_str().unwrap())
+    })
+}
